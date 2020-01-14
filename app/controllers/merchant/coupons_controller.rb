1b08fc4361af0989180a2fc2da4322ddd7e96966
Phil DeLong <philjdelong@gmail.com>
2020-01-13 18:08:36 -0700

    Add edit and update to coupons controller

--- app/controllers/merchant/coupons_controller.rb
+++ app/controllers/merchant/coupons_controller.rb
@@ -23,6 +23,21 @@ class Merchant::CouponsController < Merchant::BaseController
     @coupon = Coupon.find(params[:id])
   end
 
+  def edit
+    @coupon = Coupon.find(params[:id])
+  end
+
+  def update
+    coupon = Coupon.find(params[:id])
+    if coupon.update(coupon_params)
+      flash[:success] = "Coupon Updated!"
+      redirect_to "/merchant/coupons/#{coupon.id}"
+    else
+      flash[:error] = "Please enter valid coupon info."
+      redirect_to "/merchant/coupons/#{coupon.id}/edit"
+    end
+  end
+
   private
 
   def coupon_params
