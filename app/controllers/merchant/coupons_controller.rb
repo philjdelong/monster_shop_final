55ba91945db11f66f805b76b935cac51071fdfa7
Phil DeLong <philjdelong@gmail.com>
2020-01-13 22:12:50 -0700

    Include conditional for new or edit coupon

--- app/controllers/merchant/coupons_controller.rb
+++ app/controllers/merchant/coupons_controller.rb
@@ -10,7 +10,7 @@ class Merchant::CouponsController < Merchant::BaseController
   def create
     merchant = current_user.merchant
     coupon = merchant.coupons.create(coupon_params)
-    if coupon.save
+    if coupon.percentage < 100 && coupon.save
       flash[:success] = "Coupon Created!"
       redirect_to '/merchant/coupons'
     else
@@ -35,6 +35,18 @@ class Merchant::CouponsController < Merchant::BaseController
     else
       flash[:error] = "Please enter valid coupon info."
       redirect_to "/merchant/coupons/#{coupon.id}/edit"
+    end
+  end
+
+  def destroy
+    coupon = Coupon.find(params[:id])
+    if coupon.orders.empty?
+      coupon.destroy
+      flash[:success] = "Coupon successfully deleted."
+      redirect_to '/merchant/coupons'
+    else
+      flash[:notice] = "#{coupon.name} has been used, so it can not be deleted!"
+      redirect_to "/merchant/coupons/#{coupon.id}"
     end
   end
 
